653bed5389086fbf31faf0ae876bfe8728194a14
Peter Gustafson <petergustafson@peters-macbook-air.local>
2018-07-18 15:00:26 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Peter Gustafson on 7/17/18.
+//  Copyright © 2018 Peter Gustafson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
